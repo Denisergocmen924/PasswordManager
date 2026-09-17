26503384b3d81c9d409845f33fcd3b72048da181
denis <denisergocmen@gmail.com>
2026-09-17 12:01:11 +0300

    Changed object name

--- password_manager.py
+++ password_manager.py
@@ -6,7 +6,7 @@ class PasswordsManager:
         self.file_path = file_path
 
 
-    def load_accounts(self):
+    def load_passwords(self):
         try:
             with open(self.file_path, "r") as file:
                 return json.load(file)
@@ -14,62 +14,60 @@ class PasswordsManager:
             return []
 
 
-    def save_accounts(self, data):
+    def save_passwords(self, data):
         with open(self.file_path, "w") as file:
             json.dump(data, file, indent=4)
 
 
-    def add_account(self):
-        data = self.load_accounts()
+    def add_password(self):
+        data = self.load_passwords()
 
-        site = input("Enter the site: ")
-        username = input("Enter the username: ")
+        name = input("Enter the name: ")
         password = input("Enter the password: ")
 
-        account = {
-            "site": site,
-            "username": username,
+        password = {
+            "name": name,
             "password": password
         }
 
-        data.append(account)
-        self.save_accounts(data)
+        data.append(password)
+        self.save_passwords(data)
 
-        print("Account added successfully.")
+        print("Password added successfully.")
 
 
-    def show_accounts(self):
-        data = self.load_accounts()
+    def show_passwords(self):
+        data = self.load_passwords()
 
         if not data:
-            print("No accounts found.")
+            print("No passwords found.")
             return
 
-        for i, acc in enumerate(data, start=1):
-            print(f"{i}. {acc['site']} | {acc['username']} | {acc['password']}\n")
+        for i, pwd in enumerate(data, start=1):
+            print(f"{i}. {pwd['name']} | {pwd['password']}\n")
 
 
-    def search_account(self):
-        data = self.load_accounts()
+    def search_password(self):
+        data = self.load_passwords()
 
-        site = input("Enter site to search: ")
+        name = input("Enter name to search: ")
 
-        results = [acc for acc in data if acc["site"].lower() == site.lower()]
+        results = [pwd for pwd in data if pwd["name"].lower() == name.lower()]
 
         if not results:
-            print("No accounts found.")
+            print("No passwords found.")
             return
 
-        for i, acc in enumerate(results, start=1):
-            print(f"{i}. {acc['site']} | {acc['username']} | {acc['password']}\n")
+        for i, pwd in enumerate(results, start=1):
+            print(f"{i}. {pwd['name']} | {pwd['password']}\n")
 
 
-    def delete_account(self):
-        data = self.load_accounts()
+    def delete_password(self):
+        data = self.load_passwords()
 
-        self.show_accounts()
+        self.show_passwords()
 
-        delete_choice = input("Enter the number of the account to delete (or q to cancel): ")
+        delete_choice = input("Enter the number of the password to delete (or q to cancel): ")
 
         if delete_choice.lower() == "q":
             print("Deletion cancelled.")
@@ -78,33 +76,33 @@ class PasswordsManager:
         try:
             index = int(delete_choice) - 1
             removed = data.pop(index)
-            self.save_accounts(data)
-            print(f"{removed['site']} account deleted.")
+            self.save_passwords(data)
+            print(f"{removed['name']} password deleted.")
         except(ValueError , IndexError):
             print("Invalid selection.")
 
-manager = PasswordsManager("accounts.json")
+manager = PasswordsManager("passwords.json")
 
 while True:
-    print("\n1. Add account")
-    print("2. Show accounts")
-    print("3. Search account")
-    print("4. Delete account")
+    print("\n1. Add password")
+    print("2. Show passwords")
+    print("3. Search password")
+    print("4. Delete password")
     print("5. Exit")
 
     menu_choice = input("Select an option: ")
 
     if menu_choice == "1":
-        manager.add_account()
+        manager.add_password()
 
     elif menu_choice == "2":
-        manager.show_accounts()
+        manager.show_passwords()
 
     elif menu_choice == "3":
-        manager.search_account()
+        manager.search_password()
 
     elif menu_choice == "4":
-        manager.delete_account()
+        manager.delete_password()
 
     elif menu_choice == "5":
         break
